130a525184b9eb92456f9e618b4f583408628e9f
OrangeInky <86501049+OrangeInky@users.noreply.github.com>
2021-07-16 11:20:35 -0400

    Add files via upload

---
+++ Tab.js
@@ -0,0 +1,11 @@
+function tab (tab) {
+    document.getElementById("fight").style.display = "none"
+    document.getElementById("Upgrades").style.display = "none"
+    document.getElementById("Potion").style.display = "none"
+    document.getElementById('Player').style.display = "none"
+    document.getElementById(tab).style.display = "inline-block"
+}
+
+tab('fight');
+tab('Upgrades');
+tab('Potion');
