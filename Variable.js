c1652c071e2422b2363b86ec2b8af19f9ad4a933
OrangeInky <86501049+OrangeInky@users.noreply.github.com>
2021-07-17 16:10:20 -0400

    Add files via upload

--- Variable.js
+++ Variable.js
@@ -16,23 +16,42 @@ var GD = {
         Defense: 0,
         Name: 0,
         HP: 0,
+    },
+    STATE: {
+        DEFENSE: false,
+    },
+    MULT: {
+        ATTM: 1,
+        DEFM: 1,
+        HPM: 1,
+        GLOBAL: 1,
     }
 }
 
 var Loop = window.setInterval (function() {
     document.getElementById('CEXP').innerHTML = GD.Player.EXP;
-    document.getElementById('NEXP').innerHTML = GD.Player.MEXP;
+    document.getElementById('NEXP').innerHTML = prettify(GD.Player.MEXP - GD.Player.EXP);
     document.getElementById("CLV").innerHTML = GD.Player.LV;
     document.getElementById("HP").innerHTML = GD.Player.Health;
-    document.getElementById("ATT").innerHTML = GD.Player.Attack;
-    document.getElementById("DEF").innerHTML = GD.Player.Defense;
-    document.getElementById("FHP").innerHTML = GD.Player.HP;
+    document.getElementById("ATT").innerHTML = prettify(GD.Player.Attack);
+    document.getElementById("DEF").innerHTML = prettify(GD.Player.Defense);
+    document.getElementById("FHP").innerHTML = GD.Player.HP + "/" + GD.Player.Health;
+    if (GD.Player.EXP >= GD.Player.MEXP) {
+        GD.Player.EXP = 0;
+        GD.Player.MEXP *= 1.4;
+        GD.Player.LV += 1;
+        GD.Player.Defense = ((GD.Player.LV * GD.MULT.DEFM * GD.MULT.GLOBAL) * 0.3) + 0;
+        GD.Player.Health = ((GD.Player.LV * GD.MULT.HPM * GD.MULT.GLOBAL) * 3) + 5;
+        GD.Player.Attack = ((GD.Player.LV * GD.MULT.ATTM * GD.MULT.GLOBAL) * 1.2) + 1;
+    }
 },1000)
 
 function SPAWNM() {
     var SPAWNN = Math.floor(Math.random() * 5)
     if (SPAWNN === 0) {
-        GD.Monster1.LV = Math.floor(Math.random() * 4 + 1);
+        document.getElementById("MONSTER").style.display = "";
+        document.getElementById("WORD").style.display = "none";
+        GD.Monster1.LV = Math.floor(Math.random() * 2 + 1);
         GD.Monster1.Health = GD.Monster1.LV * 2;
         GD.Monster1.HP = GD.Monster1.Health;
         GD.Monster1.Attack = GD.Monster1.LV * 1.5;
@@ -41,16 +60,18 @@ function SPAWNM() {
         GD.Monster1.EXPD = Math.round(GD.Monster1.LV + (GD.Monster1.Health / 3) * 1.3);
         document.getElementById('MNAME').innerHTML = GD.Monster1.Name;
         document.getElementById('MLV').innerHTML = GD.Monster1.LV;
-        document.getElementById('MHP').innerHTML = GD.Monster1.HP + "/" + GD.Monster1r.Health;
+        document.getElementById('MHP').innerHTML = GD.Monster1.HP + "/" + GD.Monster1.Health;
         document.getElementById('MATT').innerHTML = GD.Monster1.Attack;
         document.getElementById('MDEF').innerHTML = GD.Monster1.Defense;
         document.getElementById('EXPDROP').innerHTML = GD.Monster1.EXPD;
     }
     if (SPAWNN === 1) {
-        GD.Monster1.LV = Math.floor(Math.random() * 4 + 1);
+        document.getElementById("MONSTER").style.display = "";
+        document.getElementById("WORD").style.display = "none";
+        GD.Monster1.LV = Math.floor(Math.random() * 2 + 1);
         GD.Monster1.Health = GD.Monster1.LV * 1;
         GD.Monster1.HP = GD.Monster1.Health;
-        GD.Monster1.Attack = GD.Monster1.LV * 3;
+        GD.Monster1.Attack = prettify(GD.Monster1.LV * 2.2);
         GD.Monster1.Defense = 0;
         GD.Monster1.Name = "Bat";
         GD.Monster1.EXPD = Math.round(GD.Monster1.LV + (GD.Monster1.Attack / 3) * 1.3);
@@ -62,5 +83,11 @@ function SPAWNM() {
         document.getElementById('EXPDROP').innerHTML = GD.Monster1.EXPD;
     }
     if (SPAWNN > 1) {
+        document.getElementById("MONSTER").style.display = "none";
+        document.getElementById("WORD").style.display = "";
     }
+}
+
+function prettify(x) {
+    return Number.parseFloat(x).toFixed(2);
 }
